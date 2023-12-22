6f51e68ccab98b0796f3572cc29ae3d8dd947a93
SuperThunderPussySmasher <cpespinar@gmail.com>
2023-12-22 18:03:56 +0100

    Day at Roche

--- src/Components/NavBarComponent/Navbar.js
+++ src/Components/NavBarComponent/Navbar.js
@@ -3,44 +3,41 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFilm, faTv, faBolt, faShapes } from '@fortawesome/free-solid-svg-icons';
 import SearchBar from './SearchBar';
 
+// Menu items as an array
+const menuItems = [
+  { path: "/movies", icon: faFilm, label: "Películas" },
+  { path: "/series", icon: faTv, label: "Series" },
+  { path: "/new", icon: faBolt, label: "Novedades" },
+  { path: "/genres", icon: faShapes, label: "Géneros" },
+];
+
 export default function Navbar(){
     return (
         <nav className="sticky top-0 bg-gray-800 text-white py-1.5 z-10">
             <div className="flex justify-between items-center w-full">
+
+                {/* Film Affinity 2 title */}
                 <div className="flex-none px-4">
                     <Link to="/" className="text-3xl font-bold hover:opacity-75 transition-opacity duration-300">
                         FilmAffinity 2
                     </Link>
                 </div>
+
+                {/* Rest of titles */}
                 <div className="flex justify-center items-center flex-grow">
                     <ul className="flex gap-x-4 ml-10">
-                        <li>
-                            <Link to="/movies" className="flex items-center hover:opacity-75 transition-opacity duration-300">
-                                <FontAwesomeIcon icon={faFilm} className="mr-2" />
-                                Películas
+                        {menuItems.map(item => (
+                          <li key={item.label}>
+                            <Link to={item.path} className="flex items-center hover:opacity-75 transition-opacity duration-300">
+                                <FontAwesomeIcon icon={item.icon} className="mr-2" />
+                                {item.label}
                             </Link>
-                        </li>
-                        <li>
-                            <Link to="/series" className="flex items-center hover:opacity-75 transition-opacity duration-300">
-                                <FontAwesomeIcon icon={faTv} className="mr-2" />
-                                Series
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/new" className="flex items-center hover:opacity-75 transition-opacity duration-300">
-                                <FontAwesomeIcon icon={faBolt} className="mr-2" />
-                                Novedades
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/genres" className="flex items-center hover:opacity-75 transition-opacity duration-300">
-                                <FontAwesomeIcon icon={faShapes} className="mr-2" />
-                                Géneros
-                            </Link>
-                        </li>
+                          </li>
+                        ))}
                     </ul>
                     <SearchBar />
                 </div>
+
             </div>
         </nav>
     );
