c96902aee7e5bd2016cb2e44c0947cf497d35106
El Yonko bravo <CPespinar@gmail.com>
2024-02-21 16:11:27 +0100

    Cast and visual improvement of MoviePage

--- src/App.js
+++ src/App.js
@@ -14,9 +14,7 @@ export default function App(){
         <Route path="/movies" element={<Peliculas />} />
       </Routes>
       <div className="text-left p-5">
-        Film Affinity 2 is the premier website to watch movies and series online. Our platform boasts the most recent films and shows in unparalleled full HD quality. To watch comprehensive movies and series for free, you can utilize the search bar at the top or follow links sorted by genre or release year on the right side of the page. Recognizing the diversity of our audience, we provide movies and series dubbed in every language on the planet, as well as subtitles. If you've been searching for the ultimate online movie and series portal, you've discovered it at Film Affinity 2.
-      <br />
-        Copyright © 2022 - 2023 Film Affinity 2. Some Rights Reserved | Film Affinity 2 does not store any files on its servers. It functions as a comprehensive content search and indexing tool. | Created with Belarus 🇧🇾.
+        Film Affinity 2 offers free access to the latest movies and series in full HD, featuring a user-friendly search and genre-based navigation. Content is available in all languages with subtitles. The site, operational from 2022 to 2023 and created with Belarusian involvement, is a powerful search and indexing tool for streaming content without hosting files on its servers.
       </div>
     </div>
     </>
